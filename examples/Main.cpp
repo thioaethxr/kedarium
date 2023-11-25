ffd49bbe6bd136fe25c48f497e92aadd5c284594
thioaethxr <thioaethxr@proton.me>
2023-11-25 23:54:59 +0100

    Creating a GLFW window

--- examples/Main.cpp
+++ examples/Main.cpp
@@ -1,4 +1,63 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+#include <string>
+
+// Constants
+constexpr unsigned int WINDOW_WIDTH  {800};
+constexpr unsigned int WINDOW_HEIGHT {600};
+const     std::string  WINDOW_TITLE  {"GLFW"};
+
 int main()
 {
+  // Initializing GLFW
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+  // Creating a GLFW Window
+  GLFWwindow* window = glfwCreateWindow(
+    WINDOW_WIDTH,
+    WINDOW_HEIGHT,
+    WINDOW_TITLE.c_str(),
+    NULL,
+    NULL
+  );
+  if (window == NULL)
+  {
+    std::cerr << "Failed to create a GLFW window!\n";
+    glfwTerminate();
+    return -1;
+  }
+  glfwMakeContextCurrent(window);
+
+  // Initializing GLEW
+  GLenum glewErr = glewInit();
+  if (GLEW_OK != glewErr)
+  {
+    std::cerr << "Failed to initialize GLEW!\n";
+    std::cerr << "Error: " << glewGetErrorString(glewErr) << '\n';
+    glfwTerminate();
+    return -1;
+  }
+
+  // Clear Color
+  GLclampf red   {0.f};
+  GLclampf green {0.f};
+  GLclampf blue  {0.f};
+  GLclampf alpha {0.f};
+  glClearColor(red, green, blue, alpha);
+
+  // Main Loop
+  while (!glfwWindowShouldClose(window))
+  {
+    glfwPollEvents();
+    glClear(GL_COLOR_BUFFER_BIT);
+    glfwSwapBuffers(window);
+  }
+
+  glfwDestroyWindow(window);
+  glfwTerminate();
   return 0;
 }
